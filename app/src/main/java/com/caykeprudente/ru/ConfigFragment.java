4d3900021cf290fbcc5f42bb4b3ac99bb8ee4525
Cayke Prudente <cayke10@gmail.com>
2016-09-28 15:16:55 -0300

    - TAB BAR FUNCIONANDO

---
+++ app/src/main/java/com/caykeprudente/ru/ConfigFragment.java
@@ -0,0 +1,21 @@
+package com.caykeprudente.ru;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by cayke on 28/09/16.
+ */
+
+public class ConfigFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.config_layout, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/caykeprudente/ru/TabBarActivity.java
@@ -0,0 +1,103 @@
+package com.caykeprudente.ru;
+
+
+import android.support.v4.app.Fragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+
+/**
+ * Created by cayke on 28/09/16.
+ */
+
+public class TabBarActivity extends AppCompatActivity {
+    private Fragment currentFragment;
+    private AHBottomNavigation bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_bar_layout);
+        initUI();
+    }
+
+    private void initUI() {
+
+        bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
+
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem(R.string.tab_1, R.drawable.icon_cardapio, R.color.colorPrimary);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem(R.string.tab_2, R.drawable.icon_cardapio, R.color.colorPrimary);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem(R.string.tab_3, R.drawable.icon_cardapio, R.color.colorPrimary);
+
+        bottomNavigation.addItem(item1);
+        bottomNavigation.addItem(item2);
+        bottomNavigation.addItem(item3);
+
+        // Set background color
+        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#FEFEFE"));
+
+        // Disable the translation inside the CoordinatorLayout
+        bottomNavigation.setBehaviorTranslationEnabled(false);
+
+        // Change colors
+        bottomNavigation.setAccentColor(Color.parseColor("#F63D2B"));
+        bottomNavigation.setInactiveColor(Color.parseColor("#747474"));
+
+
+        currentFragment = new CardapioFragment();
+        //jogar na tela
+        FragmentManager fragmentManager;
+        FragmentTransaction fragmentTransaction;
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, currentFragment);
+        fragmentTransaction.commit();
+
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                if (wasSelected) {
+                    //currentFragment.refresh();
+                    return true;
+                } else {
+                    if (position == 0) //cardapio
+                        currentFragment = new CardapioFragment();
+                    else if (position == 1) //config
+                        currentFragment = new ConfigFragment();
+                    else if (position == 2) // info
+                        currentFragment = new InfoFragment();
+                    else
+                        return false;
+
+
+                    //jogar na tela
+                    FragmentManager fragmentManager;
+                    FragmentTransaction fragmentTransaction;
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment, currentFragment);
+                    fragmentTransaction.commit();
+
+                    return true;
+                }
+            }
+        });
+    }
+
+    /**
+     * Show or hide the bottom navigation with animation
+     */
+    public void showOrHideBottomNavigation(boolean show) {
+        if (show) {
+            bottomNavigation.restoreBottomNavigation(true);
+        } else {
+            bottomNavigation.hideBottomNavigation(true);
+        }
+    }
+}
