08337b8a43a7e7058b3d7b5b03af299bc96276d7
Leandro <leandroximenes10@hotmail.com>
2016-10-07 14:29:20 -0300

    tela de consulta

--- app/src/main/java/com/caykeprudente/ru/ConsultaFragment.java
+++ app/src/main/java/com/caykeprudente/ru/ConsultaFragment.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
 
@@ -18,110 +19,60 @@ import java.util.ArrayList;
  */
 
 public class ConsultaFragment extends Fragment{
-    ListView listView;
-    CardapioListAdapter listAdapter;
 
-    ImageButton ibLeft, ibRight;
-    TextView tvTitle;
-    TabHost tabHost;
-
-    ArrayList<String> listDates;
-    int indexDate = 0;
-    int indexMeal = 0;
+    protected ImageButton btn_left;
+    protected ImageButton btn_right;
+    protected TextView text_nav_info;
+    protected View.OnClickListener single_click_listener;
+    protected ViewFlipper vf;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.cardapio_layout, container, false);
+        final View view = inflater.inflate(R.layout.consulta_layout, container, false);
 
-        listView = (ListView) view.findViewById(R.id.listViewCardapio);
 
-        // Create ArrayAdapter using the planet list.
-        listAdapter = new CardapioListAdapter(this.getContext(), R.layout.row_cardapio_layout, null);
+        btn_left = (ImageButton) view.findViewById(R.id.btn_left);
+        btn_right = (ImageButton) view.findViewById(R.id.btn_right);
+        text_nav_info = (TextView) view.findViewById(R.id.nav_info);
 
-        listView.setAdapter(listAdapter);
+        vf = (ViewFlipper) view.findViewById(R.id.vf);
 
-        tvTitle = (TextView) view.findViewById(R.id.textViewCardapioDate);
 
-        ibLeft = (ImageButton) view.findViewById(R.id.imageButtonCardapioLeft);
-        ibLeft.setOnClickListener(new View.OnClickListener() {
+        single_click_listener = new View.OnClickListener() {
+            public int num_screen = 0;
+            public int qtd_screens = 2;
+
             @Override
-            public void onClick(View view) {
-                if (indexDate > 0) {
-                    indexDate--;
-                    updateTextView();
+            public void onClick(View v) {
+                switch(v.getId()) {
+                    case R.id.btn_left:
+                        num_screen--;
+                        break;
+
+                    case R.id.btn_right:
+                        num_screen++;
+                        break;
+                }
+
+                int _mod = num_screen%qtd_screens;
+                if (_mod<0)
+                    _mod = _mod*-1;
+                vf.setDisplayedChild(_mod);
+
+                if (_mod == 0) {
+                    text_nav_info.setText("REEMBOLSO");
+                } else if (_mod == 1) {
+                    text_nav_info.setText("CRÉDITOS");
                 }
             }
-        });
+        };
 
-        ibRight = (ImageButton) view.findViewById(R.id.imageButtonCardapioRight);
-        ibRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (indexDate < listDates.size() - 1) {
-                    indexDate++;
-                    updateTextView();
-                }
-            }
-        });
+        btn_left.setOnClickListener(single_click_listener);
+        btn_right.setOnClickListener(single_click_listener);
 
-        tabHost = (TabHost) view.findViewById(R.id.tabHost);
-        tabHost.setup();
-
-        //Tab 1
-        TabHost.TabSpec spec = tabHost.newTabSpec(getResources().getString(R.string.tab_cafe));
-        spec.setContent(R.id.tab1);
-        spec.setIndicator(getResources().getString(R.string.tab_cafe));
-        tabHost.addTab(spec);
-
-        //Tab 2
-        spec = tabHost.newTabSpec(getResources().getString(R.string.tab_almoco));
-        spec.setContent(R.id.tab2);
-        spec.setIndicator(getResources().getString(R.string.tab_almoco));
-        tabHost.addTab(spec);
-
-        //Tab 3
-        spec = tabHost.newTabSpec(getResources().getString(R.string.tab_janta));
-        spec.setContent(R.id.tab3);
-        spec.setIndicator(getResources().getString(R.string.tab_janta));
-        tabHost.addTab(spec);
-
-        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-            @Override
-            public void onTabChanged(String s) {
-                if (s.equals(getResources().getString(R.string.tab_cafe)))
-                {
-                    indexMeal = 0;
-                }
-                else if(s.equals(getResources().getString(R.string.tab_almoco)))
-                {
-                    indexMeal = 1;
-                }
-                else if (s.equals(getResources().getString(R.string.tab_janta)))
-                {
-                    indexMeal = 2;
-                }
-            }
-        });
-
-        populateDateArray();
 
         return view;
-    }
-
-    private void populateDateArray ()
-    {
-        listDates = new ArrayList<>();
-        listDates.add("26/09 Segunda-feira");
-        listDates.add("27/09 Terça-feira");
-        listDates.add("28/09 Quarta-feira");
-        listDates.add("29/09 Quinta-feira");
-        listDates.add("30/09 Sexta-feira");
-    }
-
-    private void updateTextView ()
-    {
-        tvTitle.setText(listDates.get(indexDate));
     }
 
 }
---
+++ app/src/main/java/com/caykeprudente/ru/Login.java
@@ -0,0 +1,18 @@
+package com.caykeprudente.ru;
+
+/**
+ * Created by Leandro on 07/10/2016.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.consulta_credito);
+    }
+}
